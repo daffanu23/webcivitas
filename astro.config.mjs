c3d8c8b0bf0f7016d50e1d00a786cf0c40b6d40e
daffanu23 <daffanugraha058@gmail.com>
2026-09-15 18:23:26 +0700

    nambah tombol share, qr code dan sosmed lain (wa, tele, X, dan facebook)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mediacivitas.web.id',
   output: 'server',
   adapter: vercel(),
   // db() sudah kita hapus dari daftar integrasi di bawah ini:
